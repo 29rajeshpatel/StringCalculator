da8cd728386cfd99f1dbe1ee921abe30d102737f
29rajeshpatel <158438774+29rajeshpatel@users.noreply.github.com>
2024-06-09 15:49:00 +0530

    Update TestCalc.java

--- StringCalculator/src/com/inn/TestCalc.java
+++ StringCalculator/src/com/inn/TestCalc.java
@@ -1,15 +1,1 @@
-package com.inn;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-class TestCalc {
-
-	@Test
-	void test() {
-		int reslt=StringCalclator.add("1,3");
-		assertTrue(reslt==4);
-	}
-
-}
